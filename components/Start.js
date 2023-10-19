4f81e8aec0bc7b9955448d840dbf0ff7bf287533
Anastasia Fazius <AnastasiaFazius@gmx.de>
2023-10-19 13:51:58 +0200

    Adds some styling edits

--- components/Start.js
+++ components/Start.js
@@ -23,7 +23,7 @@ const Start = ({ navigation }) => {
     > */}
       <View style={styles.container}>
         {/* <Text style={styles.appTitle}>Bona Chat</Text> */}
-        <Image source={require('../assets/bona-chat.png')} style={{ width: 600, height: 600 }} />    
+        <Image source={require('../assets/bona-chat2.png')} style={styles.logoImage} />    
       </View >  
       
       <View style={[styles.container, styles.inputContainer]}> 
@@ -33,7 +33,7 @@ const Start = ({ navigation }) => {
               style={styles.icon}
               width={30}
               height={30}
-              viewBox="0 0 30 30"
+              viewBox="-8 -5 30 30"
             >
               <Path 
                 d="M12,13.2533333 C15.24,13.2533333 21.6,14.830125 21.6,18.105 L21.6,20.5308333 L2.4,20.5308333 L2.4,18.105 C2.4,14.830125 8.76,13.2533333 12,13.2533333 Z M20.64,19.5708333 L20.64,18.105 C20.64,16.0913979 15.9773097,14.2133333 12,14.2133333 C8.02269035,14.2133333 3.36,16.0913979 3.36,18.105 L3.36,19.5708333 L20.64,19.5708333 Z M12,11.36 C9.624,11.36 7.68,9.443 7.68,7.1 C7.68,4.757 9.624,2.84 12,2.84 C14.376,2.84 16.32,4.757 16.32,7.1 C16.32,9.443 14.376,11.36 12,11.36 Z M12,10.4 C13.8487889,10.4 15.36,8.90977792 15.36,7.1 C15.36,5.29022208 13.8487889,3.8 12,3.8 C10.1512111,3.8 8.64,5.29022208 8.64,7.1 C8.64,8.90977792 10.1512111,10.4 12,10.4 Z" 
@@ -117,21 +117,27 @@ const styles = StyleSheet.create({
     margin: '6%',
   }, 
 
+  logoImage: {
+    width: 600, 
+    height: 600, 
+    resizeMode: 'contain', 
+  },
+
   inputContainer: {  // white conainer with elements
     backgroundColor: 'white',    
   },
 
-  textInputSector: {
+  textInputSector: {  // box for the input
     flexDirection: 'row',
     alignItems: 'center',
     borderWidth: 1,
-    borderRadius: 5,
+    borderRadius: 8,
     width: '100%',
     borderColor: '#000000',
     height: 80,
   },
 
-  inputContent: {
+  inputContent: {  // (svg + textInput) content
     flexDirection: 'row',
     alignItems: 'center',
     flex: 1,
@@ -140,7 +146,7 @@ const styles = StyleSheet.create({
 
   icon: {
     // fontSize: 22, // the desired font size for the icon 'user'
-    marginHorizontal: 10,
+    // marginHorizontal: 10,
   },
   // iconContainer: {
   //   alignItems: 'center',  // Center vertically
@@ -149,7 +155,8 @@ const styles = StyleSheet.create({
 
   textInput: {    
     fontSize: 18,
-    padding: 5,    
+    padding: 10,  
+    width: '100%',  
   },
 
   textBgColorSelectot: {  // "Choose background color:"
@@ -181,7 +188,7 @@ const styles = StyleSheet.create({
 
   startChattingBtn: {
     backgroundColor: '#0039BF', // bright blue  
-    borderRadius: 5,    
+    borderRadius: 8,    
     width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
